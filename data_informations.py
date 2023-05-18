88b8256e9cd1351786275feda33e28716ad4ceb6
lucaluca2000 <alimomeni2000.official@gmail.com>
2023-05-18 23:52:16 +0330

    change time to SolarDate

--- data_informations.py
+++ data_informations.py
@@ -8,26 +8,5 @@ st.set_page_config(
 st.title('تحلیل مصرف برق مشترکین شهرستان بهبهان')
 
 st.markdown('''
-         
-* The market research team at AdRight is assigned the task to identify the profile of the typical customer for each treadmill product offered by CardioGood Fitness.\n
-
-* The market research team decides to investigate whether there are differences across the product lines with respect to customer characteristics.\n
-
-* The team decides to collect data on individuals who purchased a treadmill at a CardioGoodFitness retail store during the prior three months.\n
-
-* The data are stored in the CardioGoodFitness.csv file\n\n\n
-
-**The team identifies the following customer variables to study:**\n
-1- product purchased, TM195, TM498, or TM798\n
-2- gender;\n
-3- age, in years;\n
-4- education, in years;\n
-5- relationship status, single or partnered;\n
-6- annual household income ;\n
-7- average number of times the customer plans to use the treadmill each week;\n
-8- average number of miles the customer expects to walk/run each week;\n
-9- and self-rated fitness on an 1-to-5 scale, where 1 is poor shape and 5 is excellent shape.\n
-
-you can download data in this link:
-https://www.kaggle.com/datasets/saurav9786/cardiogoodfitness
+پروژه کارشناسی
          ''')
--- pages/plot.py
+++ pages/plot.py
@@ -13,15 +13,20 @@ from plotly.subplots import make_subplots
 class CollectUserInput():
     def __init__(self,df):
         self.df= df
-        self.months= months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        self.year = ['2020','2021']
+#         self.months= months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        self.months = ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند']
+
+        self.year = ['1399','1400']
     def user_input(self):
         Customer_Num  = self.df['Customer Num'].unique()
         Customer_Num_selectbox = st.sidebar.selectbox( "Customer Number : ", Customer_Num )
-        Year_slider = st.sidebar.selectbox("Year :",self.year,self.year.index('2020'))
-        if Year_slider == '2020':
-            self.months  = self.months[5:]
-        Month_slider = st.sidebar.selectbox("Month :",self.months,self.months.index('July'))
+        Year_slider = st.sidebar.selectbox("Year :",self.year,self.year.index('1400'))
+        if Year_slider == '1399':
+            self.months  = self.months[3:]
+        elif Year_slider == '1400':
+             self.months  = self.months[:9]
+
+        Month_slider = st.sidebar.selectbox("Month :",self.months,self.months.index('آذر'))
         AVG_Time_slider = st.sidebar.slider("Hours :",0,23,(13,15))
         
         data = {
@@ -34,10 +39,12 @@ class CollectUserInput():
         return features
            
     def user_input_all(self):
-        Year_slider = st.sidebar.selectbox("Year :",self.year,self.year.index('2020'))
-        if Year_slider == '2020':
-            self.months  = self.months[5:]
-        Month_slider = st.sidebar.selectbox("Month :",self.months,self.months.index('July'))
+        Year_slider = st.sidebar.selectbox("Year :",self.year,self.year.index('1400'))
+        if Year_slider == '1399':
+            self.months  = self.months[3:]
+        elif Year_slider == '1400':
+             self.months  = self.months[:9]
+        Month_slider = st.sidebar.selectbox("Month :",self.months,self.months.index('آذر'))
         AVG_Time_slider = st.sidebar.slider("Hours :",0,23,(13,15))
         
         data = {
@@ -51,15 +58,7 @@ class CollectUserInput():
     def histoplot_input(self):
         Customer_Num  = self.df['Customer Num'].unique()
         Customer_Num_selectbox = st.sidebar.selectbox( "Customer Number : ", Customer_Num )
-        # Customer_Num_selectbox1 = st.sidebar.selectbox( "Customer Number : ", Customer_Num )
-        # AVG_Time_slider = st.sidebar.slider("Hours :",0,23,(13,15))
-        
-        # data = {
-        #     'Customer': Customer_Num_selectbox,
-        #     'Time' : Customer_Num_selectbox,
 
-        #         }
-        # dat
         features =Customer_Num_selectbox
         return features
 
@@ -68,19 +67,24 @@ class Plot():
         self.df= df
         self.df2= df
         self.state= state
-        st.title('تحلیل مصرف برق مشترکین بهبان')
-        self.months = ['None','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        self.yearmonths =['June 2020', 'July 2020', 'August 2020', 'September 2020', 'October 2020', 'November 2020', 'December 2020','January 2021',
-                      'February 2021', 'March 2021', 'April 2021', 'May 2021', 'June 2021', 'July 2021', 'August 2021', 'September 2021', 'October 2021', 'November 2021', 'December 2021']
+        st.title('تحلیل مصرف برق مشترکین بهبهان')
+#         self.months = ['None','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        self.months = ['None','فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند']
+
+        self.yearmonths =['تیر 1399', 'مرداد 1399', 'شهریور 1399', 'مهر 1399', 'آبان 1399', 'آذر 1399', 'دی 1399','بهمن 1399',
+                      'اسفند 1399', 'فروردین 1400', 'اردیبهشت 1400', 'خرداد 1400', 'تیر 1400' , 'مرداد 1400', 'شهریور 1400', 'مهر 1400',
+                       'آبان 1400','آذر 1400', 'دی 1400']
         
-        self.year = ['2020','2021']
-        self.nbins = {'January':31,'February':28, 'March':31,'April':30,'May':31,'June':30,'July':31,
-                     'August':31,'September':30,'October':31,'November':30,'December':31,}
+#         self.yearmonths =['June 2020', 'July 2020', 'August 2020', 'September 2020', 'October 2020', 'November 2020', 'December 2020','January 2021', 'February 2021', 'March 2021', 'April 2021', 'May 2021', 'June 2021', 'July 2021', 'August 2021', 'September 2021', 'October 2021', 'November 2021', 'December 2021']
+#         self.year = ['2020','2021']
+#         self.nbins = {'January':31,'February':28, 'March':31,'April':30,'May':31,'June':30,'July':31,
+#                      'August':31,'September':30,'October':31,'November':30,'December':31,}
+        self.year = ['1399','1400']
+        self.nbins = {'فروردین':31,'اردیبهشت':31, 'خرداد':31,'تیر':31,'مرداد':31,'شهریور':31,'مهر':30,'آبان':30,'آذر':30,'دی':30,'بهمن':30,'اسفند':30}
 
     def histogram_box_month_plot(self):
             
         user_input=CollectUserInput(self.df)
-        # user = user_input.user_input()
                 
         if self.state == 0:
             user = user_input.user_input()
@@ -98,14 +102,29 @@ class Plot():
         newdata= data[['Date','Month','Day','Total Avg. (KW)']]
         
         data= data.iloc[:,time1:time2]
+        nbins = self.nbins[user['Month'][0]]
+
+        # df = df[df['Year']==int(user['Year'][0])]
+        sumdata = []
+        for i in range(time1+7,time2):
+            sumdata.append(df.iloc[:,i])
+        # st.write(newdata,time1)
+        # st.write(df.columns[time1+7:time2+7])
+        # st.write("heloo", )
+
+        # st.write(df.iloc[:,time1+7:time2+7])
+        df["Total selcted Time"]=pd.DataFrame(df.iloc[:,time1+7:time2+7].sum(axis=1))
+
+        fig5 = px.histogram(df, x='Day',y='Total selcted Time', nbins=nbins)
+        st.write(f"Selected Time Chart  **{user['Year'][0]} {user['Month'][0]} from {time1-2}:00 to {time2-2}:00**")
+        st.plotly_chart(fig5)
+
         newdata= pd.concat([newdata,data],axis=1)
         col = data.columns
         y_axis_val = st.selectbox('Select Y_Axis Vlaue', options= data.columns)
 
 
-        nbins = self.nbins[user['Month'][0]]
         t  = user['Time'][0]
-        # user['Time'][0]
         if user['Time'][0] == 0 and user['Time'][1] ==23: 
             y_axis_val= 'Total Avg. (KW)'
         fig1 = px.histogram(df, x='Day',y=y_axis_val,color ='Day', nbins=nbins)
@@ -113,7 +132,6 @@ class Plot():
         
         fig2 = px.box(df,x=y_axis_val) 
         fig4 = px.histogram(newdata, x='Date',y= y_axis_val)
-        # fig4 = px.box(newdata,x="Total Avg. (KW)") 
         
         del newdata
         del data
@@ -121,20 +139,23 @@ class Plot():
         st.write('---')
         col = df.iloc[:,time1+7:time2+7].columns
         if user['Time'][0] == 0 and user['Time'][1] ==23:
-            st.write('Daily average, days of ',user['Month'][0],' ',user['Year'][0])
+            st.markdown(f"Daily average, days of  **{user['Year'][0]} {user['Month'][0]}**")
+
         else:
-            st.write('Daily data of ',user['Month'][0],' ',user['Year'][0], ' for ',col[0],'hours')
+            st.markdown(f"Daily box data chart of **{user['Year'][0]} {user['Month'][0]} {y_axis_val}**")
+
         st.plotly_chart(fig1)
         st.plotly_chart(fig3)
         
         st.write('---')
-        st.write('Daily box data chart of ',user['Month'][0],' ',user['Year'][0], ' for ',col[0],"o'clock")
+        st.markdown(f"Daily box data chart of **{user['Year'][0]} {user['Month'][0]} {y_axis_val}**")
+
         st.plotly_chart(fig2)
         st.write('---')
-        st.write('The chart of different months for ',col[0],"o'clock")
-        
+        st.markdown(f"The chart of different months for data for subscriber in **{user['Year'][0]} {user['Month'][0]} {y_axis_val}**")
+
+        # st.write('The chart of different months for ',col[0],"o'clock")
         st.plotly_chart(fig4)
-        # st.plotly_chart(fig4)
       
     def histogram_box_plot(self):
         user_input=CollectUserInput(self.df)
@@ -152,10 +173,12 @@ class Plot():
 
         # y_axis_val = st.selectbox('Select Y_Axis Vlaue', options= data.iloc[:,1:].columns)
         data1 = pd.DataFrame(data.groupby(['Year','Month'])[y_axis_val].mean())
-        data1['Months']= self.yearmonths
+        data1['Months'] =['تیر 1399', 'مرداد 1399', 'شهریور 1399', 'مهر 1399', 'آبان 1399', 'آذر 1399', 'دی 1399','بهمن 1399',
+                      'اسفند 1399', 'فروردین 1400', 'اردیبهشت 1400', 'خرداد 1400', 'تیر 1400' , 'مرداد 1400', 'شهریور 1400', 'مهر 1400',
+                       'آبان 1400','آذر 1400']
         
         # the histogram and line plot for Date
-        fig = px.histogram(data, x='Date', y= y_axis_val,nbins=30)
+        fig = px.histogram(data, x='Date', y= y_axis_val)
         
         fig1 = px.line(data, x="Date", y=y_axis_val)
 
@@ -190,7 +213,6 @@ class Plot():
         st.plotly_chart(fig5)
  
     def heatmap(self):
-        
         user_input=CollectUserInput(self.df)
         time1= 0
         time2= 0
@@ -199,18 +221,15 @@ class Plot():
             data=self.df[self.df['Customer Num'] ==user['Customer'][0]]
             time1= user['Time'][0]+9
             time2= user['Time'][1]+9
+
         elif self.state==1:
             user = user_input.user_input_all()
 
             data=self.df
             time1= user['Time'][0]+9
             time2= user['Time'][1]+9
-
-        # data=self.df[self.df['Customer Num'] ==user['Customer'][0]]
-        # time1= user['Time'][0]+9
-        # time2= user['Time'][1]+9
-        newframe=[]
-        
+        data =  data[data['Year']==int(user['Year'][0])]
+       
         ind=[]
         for i in user['Month']:
             ind.append(self.months.index(i)+1)
@@ -219,108 +238,64 @@ class Plot():
         data1= data[data['Month'].isin(ind)]
         
         fig1 =px.imshow(data1.iloc[:,time1:time2].select_dtypes(include="number").corr(),text_auto=True,aspect="auto" )
-        st.plotly_chart(fig)
-        st.plotly_chart(fig1)
+        
+        st.write('---')
 
-        st.markdown('''
-            **Observation**
-            
-            ''')
+        if self.state ==0 :
+            st.markdown(f"Correlation heatmap diagram of **Annual** data for subscriber **Customer {user['Customer'][0]}  in {user['Year'][0]} from {time1-9}:00 to {time2-9}:00**")
+            st.plotly_chart(fig)
+            st.write('---')
+            st.markdown(f"Correlation heatmap diagram of **Monthly** data for subscriber **Customer {user['Customer'][0]} in {user['Month'][0]} from {time1-9}:00 to {time2-9}:00**")
+            st.plotly_chart(fig1)
+
+        elif self.state == 1:
+            st.markdown(f"Correlation heatmap diagram of **Annual** data for subscriber **in {user['Year'][0]} from {time1-9}:00 to {time2-9}:00**")
+            st.plotly_chart(fig)
+            st.write('---')
+            st.markdown(f"Correlation heatmap diagram of **Monthly** data for subscriber **in {user['Month'][0]} from {time1-9}:00 to {time2-9}:00**")
+            st.plotly_chart(fig1)
+
+
 
 def read_data():
-    df = pd.read_excel("Agricultural_Behbahan_OneHours_gregorian1.xlsx")
+    df = pd.read_csv("Agricultural_Behbahan_onehours_Fa.csv")
     return df 
      
 df= read_data()    
 # df= df.iloc[:,:]
 st.subheader('Select One 👇')
-P_selectbox = st.selectbox('',['All', 'One Customer'])
+P_selectbox = st.selectbox('',['یک مشتری', 'همه مشتری ها'])
 
-plots_selectbox = st.selectbox('',['Heatmap', 'Month Histogram','Year Histogram'])
+plots_selectbox = st.selectbox('',['Heatmap', 'نمودار ماهانه','نمودار سالانه'])
 st.markdown("""---""")  
-if P_selectbox=='All':
+if P_selectbox=='همه مشتری ها':
     plot =Plot(df,1)
 
     if plots_selectbox == 'Heatmap':
         plot.heatmap()
         st.markdown("""---""")  
         
-    elif plots_selectbox =='Month Histogram':
+    elif plots_selectbox =='نمودار ماهانه':
         plot.histogram_box_month_plot()
         st.markdown("""---""")
         
-    elif plots_selectbox =='Year Histogram':
+    elif plots_selectbox =='نمودار سالانه':
         plot.histogram_box_plot()
         st.markdown("""---""")   
 
-elif P_selectbox == 'One Customer':
+elif P_selectbox == 'یک مشتری':
     plot =Plot(df,0)
     
     if plots_selectbox == 'Heatmap':
         plot.heatmap()
         st.markdown("""---""")  
         
-    elif plots_selectbox =='Month Histogram':
+    elif plots_selectbox =='نمودار ماهانه':
         plot.histogram_box_month_plot()
         st.markdown("""---""")
         
-    elif plots_selectbox =='Year Histogram':
+    elif plots_selectbox =='نمودار سالانه':
         plot.histogram_box_plot()
         st.markdown("""---""")   
 
 
-
-
-    # def histogram_box_month_plot_all(self):
-
-    #     user_input=CollectUserInput(self.df)
-    #     user = user_input.user_input_all()
-                
-    #     data=self.df
-    #     df = data[data['Month']==self.months.index(user['Month'][0])]
-        
-    #     df = df[df['Year']==int(user['Year'][0])]
-    #     data= data.iloc[:,7:]
-    #     time1= user['Time'][0]+2
-    #     time2= user['Time'][1]+2
-    #     newdata= data[['Date','Month','Day','Total Avg. (KW)']]
-        
-    #     data= data.iloc[:,time1:time2]
-    #     newdata= pd.concat([newdata,data],axis=1)
-    #     col = data.columns
-    #     y_axis_val = st.selectbox('Select Y_Axis Vlaue', options= data.columns)
-
-
-    #     nbins = self.nbins[user['Month'][0]]
-    #     t  = user['Time'][0]
-    #     # user['Time'][0]
-    #     if user['Time'][0] == 0 and user['Time'][1] ==23: 
-    #         y_axis_val= 'Total Avg. (KW)'
-    #     fig1 = px.histogram(df, x='Day',y=y_axis_val,color ='Day', nbins=nbins)
-    #     fig3 = px.line(df, x='Day',y=y_axis_val,)
-        
-    #     fig2 = px.box(df,x=y_axis_val) 
-    #     fig4 = px.histogram(newdata, x='Date',y= y_axis_val)
-    #     # fig4 = px.box(newdata,x="Total Avg. (KW)") 
-        
-    #     del newdata
-    #     del data
-
-    #     st.write('---')
-    #     col = df.iloc[:,time1+7:time2+7].columns
-    #     if user['Time'][0] == 0 and user['Time'][1] ==23:
-    #         st.write('Daily average, days of ',user['Month'][0],' ',user['Year'][0])
-    #     else:
-    #         st.write('Daily data of ',user['Month'][0],' ',user['Year'][0], ' for ',col[0],'hours')
-    #     st.plotly_chart(fig1)
-    #     st.plotly_chart(fig3)
-        
-    #     st.write('---')
-    #     st.write('Daily box data chart of ',user['Month'][0],' ',user['Year'][0], ' for ',col[0],"o'clock")
-    #     st.plotly_chart(fig2)
-    #     st.write('---')
-    #     st.write('The chart of different months for ',col[0],"o'clock")
-        
-    #     st.plotly_chart(fig4)
-    #     # st.plotly_chart(fig4)
-    
